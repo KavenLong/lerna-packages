4d62046948cc08337fbb6522e3b0d4acc30cef65
Kaven <haiteng.long@yintech.cn>
2022-07-23 22:13:09 +0800

    feat(mian): f

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,10 @@
 const micromatch = require('micromatch')
 
-export default {
-  '*.js': (files) => {
+module.exports = {
+  'packages/**/*.js': (files) => {
     console.log(files, 7777777777777)
     // from `files` filter those _NOT_ matching `*test.js`
-    const match = micromatch.not(files, '*test.js')
+    const match = micromatch.not(files, 'packages/**/dist/*.js')
     return `eslint ${match.join(' ')}`
   }
 }
